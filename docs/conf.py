944800d8d59a3b64922243fe3b47e11388e581d9
Paul Colomiets <paul@colomiets.name>
2015-10-09 19:44:30 +0300

    Fix documentation configuration mistakes

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['lithosdomain']
+primary_domain = 'lithos'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -256,3 +257,13 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
